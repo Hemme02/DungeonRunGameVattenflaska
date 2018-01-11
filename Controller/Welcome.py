fe929777fe7c72c892ed8d084b82e521bc30395a
olssonjohanna <32932208+olssonjohanna@users.noreply.github.com>
2018-01-11 10:01:39 +0100

    welcome

---
+++ Controller/Welcome.py
@@ -0,0 +1,21 @@
+def welcomeMenu ():
+    print ("\nPlease choose one of following:\n" "1. Create a character & start an adventure\n""2. Continue with your saved character \n""3. Close program\n")
+    return
+
+print ("Welcome to Dungeon Run")
+welcomeMenu()
+
+while True:
+    choice = input("Your choice: ")
+    if choice == "1":
+        break
+        #Create character
+    elif choice == "2":
+        break
+        #Load saved character
+    elif choice == "3":
+        break
+    elif choice == "4":
+        welcomeMenu()
+    else:
+        print ("\nWrong number. Choose one between 1-3 or push 4 to see the menu again")
