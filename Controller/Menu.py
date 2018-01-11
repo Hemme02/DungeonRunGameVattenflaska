f16d61683d470727fc406e5209f4e7ec1ddbad17
Belkiz <32305578+Belkiz1701@users.noreply.github.com>
2018-01-11 10:33:17 +0100

    fixed menu to choice character

---
+++ Controller/Menu.py
@@ -0,0 +1,49 @@
+# Menu to start game
+
+def menuToStartGame ():
+
+    print ("\nSelect your character:\n" "1. Mage \n""2. Warrior  \n""3. Theif \n""4. Show more info \n""5. Close \n")
+    return
+
+
+
+print ("Start the adventure:")
+print("Menu")
+menuToStartGame ()
+
+while True:
+
+    try:
+        choice_start_game = input("Your choice: ")
+
+        # 1.Mage
+        if choice_start_game == 1:
+            break
+
+        # 2.Warrior
+        elif choice_start_game == 2:
+            break
+
+        # 3.Theif
+        elif choice_start_game == 3:
+            break
+
+
+        # 4.Show more info about characters
+        elif choice_start_game == 4:
+            break
+
+        # Close
+        elif choice_start_game == 5:
+            menuToStartGame()
+
+        else:
+            print(" Try again ! ")
+
+    except:
+        print(" Try again! ")
+
+
+
+
+
