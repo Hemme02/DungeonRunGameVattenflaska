ec1dc4cbd28e0f9fad52db5913ca175d2dd62c30
qqq <sanju_95@live.se>
2018-01-11 11:50:47 +0100

    Swagged up the welcome menu

--- Controller/Welcome.py
+++ Controller/Welcome.py
@@ -1,19 +1,41 @@
 #lägg till clear funk
 #smile
 from Controller.Menu import createMenu
+from Controller.Clear import clear_screen
+
+import time
+
+def cls():
+    print("\n" * 100)
 
 def welcomeMenu ():
+    print('________                                             __________    ')
+    print('\______ \  __ __  ____    ____   ____  ____   ____   \______   \__ __  ____   ')
+    print(' |    |  \|  |  \/    \  / ___\_/ __ \/  _ \ /    \   |       _/  |  \/    \ ')
+    print(' |    `   \  |  /   |  \/ /_/  >  ___(  <_> )   |  \  |    |   \  |  /   |  \ ')
+    print('/_______  /____/|___|  /\___  / \___  >____/|___|  /  |____|_  /____/|___|  / ')
+    print('        \/           \//_____/      \/           \/          \/           \/ ')
+    time.sleep(1)
+    print("_____________________________________________________________________________________________")
+    print('This game is a hardcore roguelike dungeon crawler, your character will be deleted upon death.')
+    time.sleep(0.5)
+    print('**** Developers: Jens, Belkiz, Johanna, Uzko, Sanju and Simon. *****')
+    print('______________________________________________________________________________________________')
+    time.sleep(2.5)
+    clear_screen()
     print ("\nPlease choose one of following:\n" "1. Create a character & start an adventure\n""2. Continue with your saved character \n""3. Close program\n")
     return
 
-def welcome(newGame_):
-    print ("Welcome to Dungeon Run")
+def welcome(newGame):
+    print ('******************************\n'"Welcome to our Dungeon Run game!" '\n******************************\n')
+    time.sleep(0.5)
+    clear_screen()
     welcomeMenu()
 
     while True:
         choice = input("Your choice: ")
         if choice == "1":
-            createMenu(newGame_)
+            createMenu(newGame)
         elif choice == "2":
             break
             #Load saved character
