7f512446ed946e4e4e1a65cd1f436c63ac8c051e
Simonn89 <32932285+Simonn89@users.noreply.github.com>
2018-01-15 10:42:01 +0100

    SuuupaSyntax!

--- roomClass.py
+++ roomClass.py
@@ -11,9 +11,6 @@ from Items.moneyBag import moneyBag
 from Items.smallTreasureChest import smallTreasureChest
 
 
-
-
-
 class Room:
     def __init__(self,):
         self.aliveMonsters = []
@@ -22,70 +19,55 @@ class Room:
         self.exit = False
         self.roomGenerator()
 
-
-
-    def roomGenerator(self):  # körs i varje rum
+#This function generates a room, calls two other funcs.
+    def roomGenerator(self):
         self.monsterGenerator()
         self.itemGenerator()
 
-
-    def exitRoom(self, i):  ## design fråga, men kan vara användbar
+#This function will empty the room and turn it into the exit-room.
+    def exitRoom(self):
         self.existingItems = []
         self.aliveMonsters = []
         self.exit = True
-
+#This function will create a startingroom that will clear the room and make the room visited.
     def startingRoom(self):
         self.existingItems = []
         self.aliveMonsters = []
         self.visited = True
 
-
+# If you visit a room the status will be changed to visited.
     def visitedRoom(self):
-
         self.visited = True
-
         return self.visited
 
-
-    def monsterGenerator(self):  # vi kan optimera random generatorn tror vi.
-
+#Function with a generator that spawns spawn mob-objects in a room.
+    def monsterGenerator(self):
         if self.Randomizer() <= 5:
             self.aliveMonsters.append(Troll())
-
         if self.Randomizer() <= 15:
             self.aliveMonsters.append(Skeleton())
-
         if self.Randomizer() >= 10:
             self.aliveMonsters.append(Orc)
-
         if self.Randomizer() <= 20:
             self.aliveMonsters.append(GiantSpider)
-
         return self.aliveMonsters
 
-
-    def itemGenerator(self):  # vi kan optimera random generatorn tror vi.
-
+#Function with a generator that spawns treasure-objects in a room.
+    def itemGenerator(self):
         if self.Randomizer() <= 40:
             self.existingItems.append(looseCoins())
-
         if self.Randomizer() <= 20:
             self.existingItems.append(moneyBag())
-
         if self.Randomizer() <= 15:
             self.existingItems.append(goldJewelry())
-
         if self.Randomizer() <= 10:
             self.existingItems.append(gemStone())
-
         if self.Randomizer() <= 5:
             self.existingItems.append(smallTreasureChest())
-
         return self.existingItems
 
-
+#Function that randomize
     def Randomizer(self):
         randomizer = random.randint(1, 100)
-
         return randomizer
 
