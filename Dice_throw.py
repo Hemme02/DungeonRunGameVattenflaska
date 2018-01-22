08c8fd2131332c1daadfb7f73903ea20cf50365c
Jens <hjerpe82@gmail.com>
2018-01-22 11:05:28 +0100

    Created dice_throw

---
+++ Dice_throw.py
@@ -0,0 +1,10 @@
+import random
+
+def dice_throw(number_of_dices):
+    result = 0
+
+    for dice in range(number_of_dices):
+        number = random.randint(1,6)
+        result += number
+
+    return result
